ce80504fe350dd7a18a3c18876785c06789be61e
shimoji0713 <120348413+shimoji0713@users.noreply.github.com>
2023-01-11 09:36:50 +0900

    arigatou 追加
    arigatouたくさん追加

--- GitTest/test01/ソース.cpp
+++ GitTest/test01/ソース.cpp
@@ -4,5 +4,11 @@ int main(void)
 {
 	printf("Hello Git World!!\n");
 	printf("arigatou\n");
+
+	printf("arigatou\n");
+	printf("arigatou\n");
+	printf("arigatou\n");
+	printf("arigatou\n");
+
 	return 0;
 }
